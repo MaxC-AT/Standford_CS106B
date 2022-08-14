2fca24d00f899e52394438bbb18fffc55234ae30
Chang-Chia-Chi <maxchang0724@gmail.com>
2022-08-14 16:23:05 +0800

    feat: complete boggle

--- Assignment_3/boggle.cpp
+++ Assignment_3/boggle.cpp
@@ -6,6 +6,8 @@
  */
 #include <iostream>    // for cout, endl
 #include <string>      // for string class
+#include <unordered_set>
+#include <vector>
 #include "backtracking.h"
 #include "gridlocation.h"
 #include "grid.h"
@@ -20,16 +22,64 @@ using namespace std;
  */
 int points(string str) {
     /* TODO: Implement this function. */
-    return 0;
+    if (str.length() <= 3) return 0;
+    return str.length() - 3;
+}
+
+int getScore(string str, Lexicon& lex, unordered_set<string>& visitedWord) {
+    /* TODO: Implement this function. */
+    if (visitedWord.find(str) != visitedWord.end()) return 0;
+    if (!lex.contains(str)) return 0;
+
+    visitedWord.insert(str);
+    return points(str);
 }
 
 /*
  * TODO: Replace this comment with a descriptive function
  * header comment.
  */
+ vector<vector<int>> getNeighbors(int row, int col, Grid<char>& board) {
+    vector<vector<int>> neighbors;
+    vector<vector<int>> dirs {{0,1}, {1,0}, {0,-1}, {-1,0}, {1,1}, {1,-1}, {-1,1}, {-1,-1}};
+    for (vector<int>& dir: dirs) {
+        int newRow = row + dir[0];
+        int newCol = col + dir[1];
+        if (newRow >= 0 && newRow < board.numRows() && newCol >= 0 && newCol < board.numCols() && board[newRow][newCol] != '#') {
+            neighbors.push_back({newRow, newCol});
+        }
+    }
+    return neighbors;
+}
+
+void scoreBoardHelp(int row, int col, int& score, string currString, Grid<char>& board, Lexicon& lex, unordered_set<string>& visitedWord) {
+    if (!currString.empty() && !lex.containsPrefix(currString)) return;
+
+    score += getScore(currString, lex, visitedWord);
+    for (vector<int> neighbor: getNeighbors(row, col, board)) {
+        int nextRow = neighbor[0];
+        int nextCol = neighbor[1];
+        char nextChar = board[nextRow][nextCol];
+        board[nextRow][nextCol] = '#';
+        scoreBoardHelp(neighbor[0], neighbor[1], score, currString + nextChar, board, lex, visitedWord);
+        board[nextRow][nextCol] = nextChar;
+    }
+    return;
+}
+
 int scoreBoard(Grid<char>& board, Lexicon& lex) {
     /* TODO: Implement this function. */
-    return 0;
+    int rows = board.numRows();
+    int cols = board.numCols();
+    unordered_set<string> visitedWord;
+
+    int score = 0;
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
+             scoreBoardHelp(r, c, score, "", board, lex, visitedWord);
+        }
+    }
+    return score;
 }
 
 /* * * * * * Test Cases * * * * * */
@@ -115,4 +165,23 @@ PROVIDED_TEST("Test scoreBoard, full board, large number of words") {
                         {'O','I','S','E'}};
 
     EXPECT_EQUAL(scoreBoard(board, sharedLexicon()), 234);
+}
+
+STUDENT_TEST("Test scoreBoard, full board, large number of words") {
+    Grid<char> board = {{'R','E','P','W'},
+                        {'A','S','N','S'},
+                        {'E','O','R','U'},
+                        {'T','O','H','E'}};
+
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()), 282);
+}
+
+STUDENT_TEST("Test scoreBoard, big full board, large number of words") {
+    Grid<char> board = {{'A','I','X','A','S'},
+                        {'G','T','T','C','M'},
+                        {'S','W','Y','R','O'},
+                        {'W','O','E','T','E'},
+                        {'S','C','D','S','N'}};
+
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()), 429);
 }
